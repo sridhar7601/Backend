27c8be27a39d8ce8a8c663ce34ffa16740af77c3
srisru <sridzar1234@gmail.com>
2023-03-20 15:14:16 +0530

    admin_updates

--- index.js
+++ index.js
@@ -6,137 +6,176 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 // Create a MySQL connection pool
-const pool = mysql.createPool({
+const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'Ss$071901',
-  database: 'courses_trail'
+  database: 'courses_trail4'
 });
 app.use(bodyParser.json());
 
-// Define API routes
+//get all courses
 app.get('/courses', (req, res) => {
-  // Fetch all courses from the courses table
-  pool.query('SELECT * FROM courses', (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    
-    // Return the courses as JSON
-    res.json(rows);
+  const query = 'SELECT * FROM courses';
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results);
   });
 });
 
-app.post('/courses/update', (req, res) => {
-  const { course_id,course_name, description } = req.body;
-  
-  // Insert a new course into the courses table
-  pool.query('INSERT INTO courses (course_id ,course_name, description) VALUES (?, ?, ?)', [course_id ,course_name, description], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    
-    // Return the ID of the new course as JSON
-    res.json({ id: result.insertId });
+// GET a course by ID
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM courses WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results[0]);
   });
 });
 
-app.get('/course_sections', (req, res) => {
-  // Fetch all courses from the courses table
-  pool.query('SELECT * FROM course_sections', (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    
-    // Return the courses as JSON
-    res.json(rows);
+// POST a new course
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+  const query = `INSERT INTO courses (name) VALUES ('${name}')`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Course added successfully');
   });
 });
 
-app.get('/course_sections/:course_id', (req, res) => {
-  const { course_id } = req.params;
-  
-  // Fetch all sections for the specified course from the course_sections table
-  pool.query('SELECT * FROM course_sections WHERE course_id = ?', [course_id], (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    
-    // Return the sections as JSON
-    res.json(rows);
+// PUT (update) an existing course by ID
+app.put('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const { name } = req.body;
+  const query = `UPDATE courses SET name = '${name}' WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Course updated successfully');
   });
 });
 
-app.get('/paragraphs', (req, res) => {
-  // Fetch all courses from the courses table
-  pool.query('SELECT * FROM paragraphs', (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    
-    // Return the courses as JSON
-    res.json(rows);
+// DELETE a course by ID
+app.delete('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM courses WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Course deleted successfully');
   });
 });
 
-app.get('/paragraphs/:section_id', (req, res) => {
-  const { section_id } = req.params;
-  
-  // Fetch all paragraphs for the specified section from the paragraphs table
-  pool.query('SELECT * FROM paragraphs WHERE section_id = ?', [section_id], (err, rows) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    
-    // Return the paragraphs as JSON
-    res.json(rows);
+//weeks
+//get all for weeks 
+app.get('/weeks', (req, res) => {
+  const query = 'SELECT * FROM weeks';
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results);
   });
 });
 
-//for updating paragraphs
-app.put('/paragraphs/:paragraph_id', (req, res) => {
-  const { paragraph_id } = req.params;
-  const { content } = req.body;
-
-  // Update the specified paragraph in the paragraphs table
-  pool.query('UPDATE paragraphs SET paragraph_content = ? WHERE paragraph_id = ?', [content, paragraph_id], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-
-    // Return the number of affected rows as JSON
-    res.json({ rowsAffected: result.affectedRows });
+// GET all weeks for a course
+app.get('/courses/:course_id/weeks', (req, res) => {
+  const courseId = req.params.course_id;
+  const query = `SELECT * FROM weeks WHERE course_id = ${courseId}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results);
   });
 });
 
-//for deleting paragraphs
-app.delete('/paragraphs/:paragraph_id', (req, res) => {
-  const { paragraph_id } = req.params;
+// GET a week by ID
+app.get('/weeks/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM weeks WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results[0]);
+  });
+});
 
-  // Delete the specified paragraph from the paragraphs table
-  pool.query('DELETE FROM paragraphs WHERE paragraph_id = ?', [paragraph_id], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
+// POST a new week for a course
+app.post('/courses/:course_id/weeks', (req, res) => {
+  const courseId = req.params.course_id;
+  const {number, title } = req.body;
+  const query = `INSERT INTO weeks (course_id, number, title) VALUES (${courseId}, ${number}, '${title}')`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Week added successfully');
+  });
+});
 
-    // Return the number of affected rows as JSON
-    res.json({ rowsAffected: result.affectedRows });
+// PUT (update) an existing week by ID
+app.put('/weeks/:id', (req, res) => {
+  const id = req.params.id;
+  const { number, title } = req.body;
+  const query = `UPDATE weeks SET number = ${number}, title = '${title}' WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Week updated successfully');
+  });
+});
+
+// DELETE a week by ID
+app.delete('/weeks/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM weeks WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Week deleted successfully');
+  });
+});
+
+//subtopic
+// GET all subtopics for a week
+app.get('/weeks/:weekId/subtopics', (req, res) => {
+  const weekId = req.params.weekId;
+  const query = `SELECT * FROM subtopics WHERE week_id = ${weekId}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+// GET a subtopic by ID
+app.get('/subtopics/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM subtopics WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send(results[0]);
+  });
+});
+
+// POST a new subtopic for a week
+app.post('/weeks/:weekId/subtopics', (req, res) => {
+  const weekId = req.params.weekId;
+  const { title } = req.body;
+  const query = `INSERT INTO subtopics (week_id, title) VALUES (${weekId}, '${title}')`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Subtopic added successfully');
+  });
+});
+
+// PUT (update) an existing subtopic by ID
+app.put('/subtopics/:id', (req, res) => {
+  const id = req.params.id;
+  const { title } = req.body;
+  const query = `UPDATE subtopics SET title = '${title}' WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Subtopic updated successfully');
+  });
+});
+
+// DELETE a subtopic by ID
+app.delete('/subtopics/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM subtopics WHERE id = ${id}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.send('Subtopic deleted successfully');
   });
 });
 
